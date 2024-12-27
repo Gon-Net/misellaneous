c36d03a4c797924f3ef13dd22239ed0a034f4b95
jcori <gonzalocori@gmail.com>
2024-12-26 22:23:37 -0400

    Add go_router config

---
+++ lib/presentation/screens/screens.dart
@@ -0,0 +1,4 @@
+export 'package:widget1_app/presentation/home_screen.dart';
+export 'package:widget1_app/presentation/screens/buttons/buttons_screen.dart';
+export 'package:widget1_app/presentation/screens/cards/cards_screen.dart';
+
