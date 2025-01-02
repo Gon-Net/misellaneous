b0373fd0550d40cdb5c3dbaa3cd71271422dc660
jcori <gonzalocori@gmail.com>
2025-01-02 17:51:34 -0400

    Add snackbar and dialog finality

---
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class ProgressScreen extends StatelessWidget {
+
+  static const name = 'progress_screen';
+
+  const ProgressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Progress Indicators'),
+      ),
+      body: _ProgressView(),
+    );
+  }
+}
+
+class _ProgressView extends StatelessWidget {
+  const _ProgressView();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Column(
+        children: [
+          SizedBox(height: 30,),
+          Text('Circular progress indicator 1'),
+          SizedBox(height: 10,),
+          CircularProgressIndicator(strokeWidth: 2, backgroundColor: Colors.black45,),
+          SizedBox(height: 20,),
+          Text('Circular progress indicator 2 - Controlado'),
+          SizedBox(height: 10,),
+          _ControllerProgressIndicator()
+        ],
+      ),
+    );
+  }
+}
+
+class _ControllerProgressIndicator extends StatelessWidget {
+  const _ControllerProgressIndicator();
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300), (value){
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot){
+
+        final progressValue = snapshot.data ?? 0;
+
+        return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator(value: progressValue, strokeWidth: 2, backgroundColor: Colors.black12,),
+            const SizedBox(width: 20,),
+            Expanded(
+              child: LinearProgressIndicator(value: progressValue),
+            )
+          ],
+        ),
+      );
+      },
+    );
+  }
+}
---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:widget1_app/config/router/app_router.dart';
+
+class SnackbarScreen extends StatelessWidget {
+
+  static const name = 'snackbar_screen';
+
+  const SnackbarScreen({super.key});
+
+  void showCustomSnackbar(BuildContext context){
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+
+    final snackbar = SnackBar(
+      content: const Text('Hola mundo...!!!'),
+      action: SnackBarAction(label: 'OK !', onPressed: (){
+      
+      }),
+      duration: const Duration(seconds: 2),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(snackbar);
+
+  }
+
+  void openDialog(BuildContext context){
+    showDialog(
+      context: context, 
+      barrierDismissible: false,
+      builder: (context) => AlertDialog(
+        title: const Text('Estas seguro??'),
+        content: const Text('Esto es un ejemplo, de show dialog para mostrar mensajes resumidos el contenido del aplicatikvo'),
+        actions: [
+          TextButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Cancelar')
+          ),
+          FilledButton(
+            onPressed: () => context.pop(), 
+            child: const Text('Aceptar')
+          )
+        ],
+      )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Snackbars y Dialogos'),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FilledButton.tonal(
+              onPressed: (){
+                showAboutDialog(
+                  context: context,
+                  children: [
+                    const Text('Esto es un ejemplo, de show dialog para mostrar mensajes resumidos el contenido del aplicatikvo')
+                  ]
+                );
+            }, 
+            child: const Text('Licencias usadas...')),
+            FilledButton.tonal(
+              onPressed: () => openDialog(context), 
+            child: const Text('Mostrar dialogo...')),
+          ],
+        ),
+      ),
+      
+      floatingActionButton: FloatingActionButton.extended(         
+        label: const Text('Mostrar Snackbar'),
+        icon: const Icon(Icons.remove_red_eye_outlined),
+        onPressed: () => showCustomSnackbar(context),
+      ),
+    );
+  }
+}
